e39a241bbab2cb3329a59c7a08d7bf90d61919f4
Marzieh Morovatpasand <marzieh312@gmail.com>
2019-03-12 14:53:57 +1100

    added bootstrap for layout

--- ui/src/components/layout/AppLayout.js
+++ ui/src/components/layout/AppLayout.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import {
+  Nav,
   Navbar, 
   NavbarBrand,
-  Container
+  NavbarToggler,
+  Container,
+  Collapse,
+  NavItem,
+  NavLink
 } from 'reactstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import styles from './AppLayout.module.scss'
 
-const AppLayout = (props) => (
-  <div className={styles.appLayout}>
-    <Navbar color="light" light expand="md"  fixed="top">
-      <LinkContainer to="/">
-        <NavbarBrand href="/">
-          <span>Weather Report</span>
-        </NavbarBrand>
-      </LinkContainer>
-    </Navbar>
-    <Container>
-      {props.children}
-    </Container>
 
-  </div>
-)
+class AppLayout extends React.Component {
+  state = {
+    collapsed: false
+  }
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.appLayout}>
+        <Navbar color="light" light expand="md" fixed="top">
+          <LinkContainer to="/">
+            <NavbarBrand href="" className="mr-auto">
+              Hello CMS
+            </NavbarBrand>
+          </LinkContainer>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={this.state.collapsed} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/faq">FAQ</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Container>
+          {this.props.children}
+        </Container>
+    
+      </div>
+    )
+  }
+} 
 
 export default AppLayout;
--- ui/src/index.js
+++ ui/src/index.js
@@ -5,6 +5,7 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
